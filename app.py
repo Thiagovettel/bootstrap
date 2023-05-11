1222131ecaeeba37ad1323caf119ad324928a891
Thiago Alves Rocha <thiago.arochasup@senacsp.edu.br>
2023-05-11 11:53:12 -0300

    barra menu

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask,template_rendered,request
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Ola Mundo'
+    return render_template ('index.html')
 
-if __name__ == '__main__':
+
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
+
+@app.route('/cursos')
+
+def cursos():
+    return render_template('cursos.html')
+
+@app.route('/login')
+
+def login():
+    return render_template('login.html')
+
+@app.route('/contato')
+
+def contato():
+    return render_template('contato.html')
+
+@app.route('/sobre_ead')
+
+def sobre_ead():
+    return render_template('sobre_ead.html')
+
+
+if __name__ =='__main__':
     app.run(debug=True)
+    
